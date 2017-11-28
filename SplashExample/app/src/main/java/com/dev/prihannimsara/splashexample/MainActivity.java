aa4d68c0b3ec53fd16f002b960852b480e550c84
Prihan Nimsara <iamprihan@gmail.com>
2017-11-28 11:19:10 +0530

    test one

--- SplashExample/app/src/main/java/com/dev/prihannimsara/splashexample/MainActivity.java
+++ SplashExample/app/src/main/java/com/dev/prihannimsara/splashexample/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //code anythng here for your app
+        //code anythng here for your application
     }
 }
